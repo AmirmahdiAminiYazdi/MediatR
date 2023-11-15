32d1e57ca266b4f97322a3413003cfc61372f3d1
amir mahdi amini yazdi <amirmahdi.amini@yahoo.com>
2023-11-15 21:49:29 +0330

    initial Project

---
+++ Library/DataAccess/DataAccess.cs
@@ -0,0 +1,31 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.DataAccess
+{
+    public class DataAccess : IDataAccess
+    {
+        private List<PersonModels> people = new();
+        public DataAccess()
+        {
+            people.Add(new PersonModels { Id = 1, FirstName = "Amir Mahdi", LastName = "Amini Yazdi" });
+            people.Add(new PersonModels { Id = 2, FirstName = "Kim", LastName = "JF" });
+
+        }
+        public List<PersonModels> GetPeople()
+        {
+            return people;
+        }
+        public PersonModels InstertPerson(string firstName, string lastName)
+        {
+            PersonModels person = new() { FirstName = firstName, LastName = lastName };
+            person.Id = people.Max(x => x.Id) + 1;
+            people.Add(person);
+            return person.;
+        }
+    }
+}
---
+++ Library/DataAccess/IDataAccess.cs
@@ -0,0 +1,10 @@
+﻿using Library.Models;
+
+namespace Library.DataAccess
+{
+    public interface IDataAccess
+    {
+        List<PersonModels> GetPeople();
+        PersonModels InstertPerson(string firstName, string lastName);
+    }
+}
